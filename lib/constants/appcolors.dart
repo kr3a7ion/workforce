62c1a778f8bf355eaffc20e19448ff57975319ba
Gideon <kr3a7ion@gmail.com>
2022-03-14 13:21:34 +0100

    update

--- lib/constants/appcolors.dart
+++ lib/constants/appcolors.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
+  static const aa = Color(0xffebeff3);
 
+  static const ab = Color(0xffacd2fe);
+
+  static const ac = Color(0xff0e0f10);
+
+  static const ad = Color(0xff1b93fb);
+
+  static const ae = Color(0xff0f71ed);
+
+  static const af = Color(0xff505658);
+
+  static const ag = Color(0xffa6a9b1);
+
+  static const ah = Color(0xffc67b22);
 }
